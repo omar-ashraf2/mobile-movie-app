8a1b7643b8fc6681f252bd49c6fcd905b7982869
Omar Ashraf <oashraf304@gmail.com>
2025-03-18 18:44:28 +0200

    saved and profile pages placeholder

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,14 +1,17 @@
+import { icons } from "@/constants/icons";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
 
 const profile = () => {
   return (
-    <View>
+    <View className="bg-primary flex-1 px-10">
+      <View className="flex justify-center items-center flex-1 flex-col gap-5">
+        <Image source={icons.person} className="size-10" tintColor="#fff" />
+        <Text className="text-2xl font-bold text-gray-500">John Doe</Text>
+      </View>
       <Text>profile</Text>
     </View>
   );
 };
 
 export default profile;
-
-const styles = StyleSheet.create({});
--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -1,14 +1,17 @@
-import { StyleSheet, Text, View } from "react-native";
+import { icons } from "@/constants/icons";
 import React from "react";
+import { Image, Text, View } from "react-native";
 
 const saved = () => {
   return (
-    <View>
-      <Text>saved</Text>
+    <View className="bg-primary flex-1 px-10">
+      <View className="flex justify-center items-center flex-1 flex-col gap-5">
+        <Image source={icons.save} className="size-10" tintColor="#fff" />
+        <Text className="text-2xl font-bold text-gray-500">Saved</Text>
+      </View>
+      <Text>profile</Text>
     </View>
   );
 };
 
 export default saved;
-
-const styles = StyleSheet.create({});
